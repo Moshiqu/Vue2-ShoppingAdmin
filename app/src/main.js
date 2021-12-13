3feea7f2d3c389078be657f9443809a2a89f194a
Moshiqu <912323520@qq.com>
2021-12-13 17:24:39 +0800

    引入主页其他组件

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 // 引入路由
 import router from "@/router";
+// 三级联动组件--- 引入全局组件
+import TypeNav from "@/pages/Home/TypeNav"
+// 第一个参数, 全局组件的名字, 第二个参数, 哪一个组件--- 注册全局组件
+Vue.component(TypeNav.name,TypeNav)
 
 new Vue({
   render: h => h(App),
@@ -9,3 +13,5 @@ new Vue({
   // 注册路由信息: 当这里书写router的时候, 组件身上都拥有$route属性
   router
 }).$mount('#app')
+
+
--- app/src/pages/Home/index.vue
+++ app/src/pages/Home/index.vue
@@ -1,12 +1,36 @@
 <template>
     <div>
-        首页
+        <!-- 三级联动组件: 三级联动组件已经注册为全局组件, 这里不需要再引入 -->
+        <TypeNav/>
+        <ListContainer/>
+        <TodayRecommend/>
+        <Rank/>
+        <Like/>
+        <Floor/>
+        <Floor/>
+        <Brand/>
     </div>
 </template>
 
 <script>
+import ListContainer from "@/pages/Home/ListContainer"
+import TodayRecommend from '@/pages/Home/TodayRecommend'
+import Rank from '@/pages/Home/Rank'
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+
+
 export default{
-    name:''
+    name: "",
+    components:{
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
+}
 }
 </script>
 
